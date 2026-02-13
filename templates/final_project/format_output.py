25fc96dab45da8733558a634742b3c8d01a48376
jasiunes <69402983+jasiunes@users.noreply.github.com>
2026-02-13 12:43:40 +0100

    Update format_output.py

--- templates/final_project/format_output.py
+++ templates/final_project/format_output.py
@@ -1,1 +1,7 @@
+def format_emotion_output(response):
+    dominant = response.get("dominant_emotion")
 
+    if dominant is None:
+        return "Invalid text! Please try again."
+
+    return f"The dominant emotion is: {dominant}"
