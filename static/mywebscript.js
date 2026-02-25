57c77f1d6ecfdc590c7351bfb84a247c4d8f6db7
jasiunes <69402983+jasiunes@users.noreply.github.com>
2026-02-25 07:41:27 +0100

    Update mywebscript.js

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,6 +1,5 @@
 let RunSentimentAnalysis = ()=>{
     textToAnalyze = document.getElementById("textToAnalyze").value;
-
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
